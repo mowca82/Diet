2bdb95ef4a8eb84ec63f7d98fba299129e9394e0
Marcin Owczarek <108987482+mowca82@users.noreply.github.com>
2022-11-07 21:55:17 +0100

    Add files via upload

---
+++ Diet/main.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    document.getElementById("DJ").innerHTML = tab3[Math.floor(Math.random() * tab3.length)];
+
+}
+const tab3 = ["Marchew", "Dynia", "Kalafior", "Ziemniak", "Brokuł", "Pasternak"];
